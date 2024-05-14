5491aadf7e935a7d27e7bdacaa0cd390842eeb05
Golis <bartosz.golis@gmail.com>
2024-05-14 15:21:17 +0200

    fixed

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
+import numpy as np
 import cv2
 
-# Utworzenie obiektu BackgroundSubtractorMOG2
-background_subtractor = cv2.createBackgroundSubtractorMOG2()
-
-# Uruchomienie kamery
 cap = cv2.VideoCapture(0)
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while True:
-    # Odczytanie klatki z kamery
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Zastosowanie BackgroundSubtractorMOG2 do klatki
-    fg_mask = background_subtractor.apply(frame)
+    fgmask = fgbg.apply(frame)
+    
+    # Wykrycie krawędzi na masce
+    edges = cv2.Canny(fgmask, 30, 150)
 
-    # Wyświetlenie klatki i maske na obrazie
-    cv2.imshow('Original Frame', frame)
-    cv2.imshow('Foreground Mask', fg_mask)
+    # Nałożenie krawędzi na oryginalną klatkę
+    frame_with_edges = cv2.bitwise_and(frame, frame, mask=edges)
 
-    # Przerwanie pętli po naciśnięciu klawisza 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Wyświetlenie oryginalnej klatki z nałożonymi krawędziami
+    cv2.imshow('frame with edges', frame_with_edges)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
 
-# Zamknięcie kamery i okien
 cap.release()
 cv2.destroyAllWindows()
